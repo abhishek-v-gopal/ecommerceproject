4ccc3a63458ac21ef2c2718d2ed82508fa970d11
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-01-24 22:30:47 +0530

    added

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -77,6 +77,7 @@ module.exports={
                         pipeline:[
                             {
                                $match:{
+                                
                                 $expr:{
                                     $in:['$_id',"$$productslist"]
                                 }
